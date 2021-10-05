105b0c636896c0f6391a24fe83456874efaf198d
Davo Minasyan <davo.minas666@gmail.com>
2021-10-05 16:42:58 +0400

    first commit

---
+++ src/lesson2/Example2.java
@@ -0,0 +1,22 @@
+package lesson2;
+
+public class Example2 {
+
+    public static void main(String[] args) {
+
+        int a, a2;
+        a = 5;
+        a2 = a;
+        System.out.println("a=" + a);
+        a = a * 2;
+        System.out.println("a=" + a);
+        System.out.println("a2=" + a2);
+        if (a >= 2) {
+            int y = 20;
+            System.out.println("a մեծ կամ հավասար է a2-ից");
+
+
+        }
+
+    }
+}
---
+++ src/lesson2/Example3.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class Example3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("lux" + i);
+        }
+    }
+}
